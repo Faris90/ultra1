94104d47a7353c8cf458941cefa8bd70b2a6eec7
MK Games <37133977+MkGamesdev@users.noreply.github.com>
2018-11-09 14:28:29 +1100

    Update console-plus.js

--- run/console-plus.js
+++ run/console-plus.js
@@ -176,7 +176,7 @@ const config = require('./console-plus.json');
         perMessageDeflate: false,
         maxPayload: 4096,
         protocolVersion: 8,
-        origin:'http://' + config.ip + ':' + config.port,
+        origin:'http://' + config.ip + ':' +  $PORT,
         autoAcceptConnections: false
     };
 
